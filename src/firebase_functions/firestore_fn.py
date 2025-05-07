88ac5795aa4da81c95a6c9d63c8beb2470822ff8
jniebur <jniebur@owletcare.com>
2025-05-07 13:35:11 -0500

    NO-JIRA: fix logging

--- src/firebase_functions/firestore_fn.py
+++ src/firebase_functions/firestore_fn.py
@@ -212,8 +212,9 @@ def _firestore_endpoint_handler(
     func = _core._with_init(func)
 
     if event_type.endswith(".withAuthContext"):
-        _logging.info(f"event_type: {event_type}")
-        _logging.info(f"event_attributes: {event_attributes}")
+        _logging.warning(f"event_type: {event_type}")
+        _logging.warning(f"event_attributes: {event_attributes}")
+
         event_auth_type = event_attributes["authtype"]
         event_auth_id = event_attributes["authid"]
         database_event_with_auth_context = AuthEvent(**vars(database_event),
